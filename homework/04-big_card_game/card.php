396d0fa6f66a59ac9c9b3a185ec72721c792b46b
jorions <jared.orion.selcoe@gmail.com>
2015-10-04 20:55:43 -0500

    Built card game.
    Still need to add DocBook comments

--- homework/04-big_card_game/card.php
+++ homework/04-big_card_game/card.php
@@ -200,14 +200,21 @@ class Deck extends Card {
      * Create the deck using the permitted suits
      */
     public function __construct() {
+
+        // Iterate through the allowed suits and automatically build a deck
         foreach($this->allowedSuits as $suit) {
+
             for($i=0; $i<14; $i++) {
+
                 $this->deck[] = new Card($suit, $i);
             }
         }
     }
 
     /*
+    ###################################################################################################################################
+    ###################################### ALTERNATE METHOD THAT MAY BE CLOSER TO INSTRUCTIONS ########################################
+    ###################################################################################################################################
     public function __construct($cardArray) {
         $this->deck = $cardArray();
     }
@@ -234,7 +241,7 @@ class Deck extends Card {
 }
 
 
-class Player extends Card {
+class Player extends Deck {
 
     /**
      * The player's name
@@ -248,42 +255,318 @@ class Player extends Card {
      */
     public $hand = array();
 
+    /**
+     * A string containing every card in the players hand in an HTML-ready format
+     * @var string
+     */
+    public $handRender = "";
+
+    public $score = 0;
+
+    /**
+     * Create a player with a name and empty hand of cards
+     * @param string $name The player's name
+     */
     public function __construct($name) {
 
         $this->name = $name;
-        $this->hand = array();
     }
 
-
+    /*
+     * ###################################################################################################################################
+     * ######################################## WHEN IS IT APPROPRIATE TO USE "STATIC" METHODS? ##########################################
+     * ###################################################################################################################################
+     */
+    /**
+     * Add a card to the player's hand
+     * @param Card $card A card object the player receives to put in their hand
+     * @return void
+     */
     public function receiveCard(Card $card) {
+
         $this->hand[] = $card;
     }
 
+    /*
+    ###################################################################################################################################
+    ############################################### WHY DOES THIS NOT WORK? ###########################################################
+    ###################################################################################################################################
     public function showHand() {
         foreach($this->hand as $card) {
             $card->render();
+        }
+    }
+    */
+
+    /**
+     * Combine all of the player's cards into 1 string that is HTML-ready
+     * @return string
+     */
+    public function showHand() {
+
+        // Start by adding the HTML-formatted player's name to the output string
+        $this->handRender = "<div class='hand'><div class='name'><b>$this->name's Cards</b></div>";
+
+        // Iterate through the player's hand and add the HTML-formatted string representation of the card to the output string
+        foreach($this->hand as $card) {
+
+
+            $this->handRender .= "<div class='card-$card->color'>
+            <div class='cardValueTopLeft'>$card->finalRank$card->icon</div>
+            <div class='cardValueTopRight'>$card->finalRank$card->icon</div>
+            <div class='cardSuitMiddle'>$card->icon</div>
+            <div class='cardValueBottomLeft'>$card->finalRank$card->icon</div>
+            <div class='cardValueBottomRight'>$card->finalRank$card->icon</div>
+            </div>";
+        }
+
+        $this->handRender .= "</div>";
+
+        return $this->handRender;
+    }
+}
+
+class Dealer extends Player {
+
+    /**
+     * An array of all of the players in the game
+     * @var array
+     */
+    public $players = array();
+
+    /**
+     * The number of cards that each player should be dealt
+     * @var int
+     */
+    public $numCards;
+
+    /**
+     * A string containing every card in the deck in an HTML-ready format
+     * @var string
+     */
+    public $deckRender = "";
+
+    /**
+     * A string containing all player's scores in an HTML-ready format
+     * @var string
+     */
+    public $scoreRender = "";
+
+    /**
+     * The array of all winners of the game
+     * @var array
+     */
+    public $winnerArray = array();
+
+    /**
+     * The highest score of any of the players
+     * @var int
+     */
+    public $highScore = 0;
+
+    /*
+     * ###################################################################################################################################
+     * #### IS IT BAD TO USE THE SAME VARIABLE NAME HERE AND IN THE DECK PARENT CLASS? THE VARIABLE DOESN'T EVEN SEEM NECESSARY HERE #####
+     * ###################################################################################################################################
+     */
+    //public $deck;
+
+    public function __construct($allPlayers, $num, Deck $deck) {
+        $this->players = $allPlayers;
+        $this->numCards = $num;
+
+        /*
+         * ###################################################################################################################################
+         * ########################### WHY IS THIS REQUIRED? ISN'T THE DECK VARIABLE INHERITED FROM THE PARENT CLASS? ########################
+         * ###################################################################################################################################
+         */
+        $this->deck = $deck;
+    }
+
+
+    public function setupGame() {
+
+        if (count($this->players) > count($this->deck->deck) / $this->numCards) {
+
+            // Initialize variables to count how many more decks are needed
+            $extraCardsNeeded = (count($this->players) * $this->numCards) - count($this->deck->deck);
+            $extraDecksNeeded = ceil($extraCardsNeeded / count($this->deck->deck));
+
+            // Initialize variables to hold new decks to be added
+            $extraCards = array();
+
+            // Create additional decks to add
+            for ($i = 0; $i < $extraDecksNeeded; $i++) {
+
+                // Loop through deck and build additional cards array
+                foreach ($this->deck->deck as $card) {
+                    $extraCards[] = $card;
+                }
+            }
+
+            // Add new decks to original deck
+            $this->deck->deck = array_merge($this->deck->deck, $extraCards);
+
+            // Tell users what happened
+            if ($extraDecksNeeded == 1) {
+                return "<div class='message'>There are too many players so we have added 1 more deck</div>";
+            } else {
+                return "<div class='message'>There are too many players so we have added  $extraDecksNeeded more decks</div>";
+            }
+        }
+    }
+
+    public function deals() {
+        $this->deck->shuffle();
+
+        foreach ($this->players as $player) {
+
+            for ($i = 0; $i < $this->numCards; $i++) {
+
+                $player->receiveCard($this->deck->getCard());
+            }
+        }
+    }
+
+    public function scoreGame() {
+        foreach($this->players as $player) {
+            foreach($player->hand as $card) {
+                $player->score += $card->rank;
+            }
+        }
+    }
+
+    public function showDeck() {
+
+        $this->deckRender = "<div class='hand'>";
+
+        // Iterate through the deck and add an HTML-formatted string representation of the card image to the output string
+        for($i=0; $i<count($this->deck->deck); $i++) {
+            $this->deckRender .= "<div class='card-black'><img src='images/card.png' /></div>";
+        }
+
+        $this->deckRender .= '</div>';
+        return $this->deckRender;
+    }
+
+    public function showScores() {
+
+        foreach($this->players as $player) {
+            $this->scoreRender .= "<div class='hand'><div class='name'><b>$player->name's Score</b></div>
+            <div class='score'>$player->score</div></div>";
+        }
+
+        return $this->scoreRender;
+    }
+
+
+    public function determineWinners() {
+        foreach($this->players as $player) {
+            if($player->score > $this->highScore) {
+                $this->highScore = $player->score;
+                $this->winnerArray = array();
+                $this->winnerArray[] = $player->name;
+            } else if($player->score == $this->highScore) {
+                $this->winnerArray[] = $player->name;
+            }
+        }
+        return $this->winnerArray;
+    }
+
+    public function showWinners() {
+
+        if(count($this->winnerArray) == 1) {
+            return "<div class='score'>" . $this->winnerArray[0] . '! With a score of ' . $this->highScore . '!</div>';
+        } else if(count($this->winnerArray) == 2) {
+            return "<div class='score'>Both " . $this->winnerArray[0] . ' and ' . $this->winnerArray[1] . ' tied with a score of ' . $this->highScore . '!</div>';
+        } else if(count($this->winnerArray) >= 3) {
+            $tempString = "";
+            foreach($this->winnerArray as $winner) {
+                $tempString .= $winner . ", ";
+            }
+            $tempString = substr($tempString, 0, -2);
+            return "<div class='score'>" . $tempString . ' all won with a score of ' . $this->highScore . '!</div>';
         }
     }
 }
 
 
 
-
-
-
-
 $deck = new Deck();
+
 $jared = new Player("Jared");
+$jacy = new Player("Jacy");
+$jeremy = new Player("Jeremy");
 
-echo "<div class='table'><div class='hand'>";
+$allPlayers = array($jared, $jacy, $jeremy);
 
-$i = 0;
-foreach($deck->deck as $card) {
-    echo $card->render();
-    if($i<3) {
-        $jared->receiveCard($card);
-        $i++;
+$dealer = new Dealer($allPlayers, 3, $deck);
+
+echo $dealer->setupGame();
+
+echo "<div class='table'>";
+
+    echo "<div class='headline'><b>The Deck</b></div>";
+
+    echo $dealer->showDeck();
+
+    echo "<div class='divider'></div>";
+
+    $dealer->deals();
+
+    /*
+    ###################################################################################################################################
+    ####################################### USE THIS APPROACH IF THERE IS NO SHOWHAND() METHOD ########################################
+    ###################################################################################################################################
+    echo "<div class='table'><div class='hand'>";
+
+    foreach($jared->hand as $card) {
+        echo $card->render();
     }
-}
 
-$jared->showHand();
+    echo "</div></div>";
+    */
+
+    echo "<div class='headline'>Player's Hands</div>";
+
+    foreach($allPlayers as $player) {
+        echo $player->showHand();
+    }
+
+    echo "<div class='divider'></div>";
+
+    echo "<div class='headline'><b>Score</b></div>";
+
+    $dealer->scoreGame();
+
+    $dealer->determineWinners();
+
+    echo $dealer->showScores();
+
+    echo "<div class='divider'></div>";
+
+    echo "<div class='headline'><b>And The Winner Is...</b></div>";
+
+    echo $dealer->showWinners();
+
+
+echo "</div>";
+/*
+ * ###################################################################################################################################
+ * ##################################################### OTHER QUESTIONS #############################################################
+ * ###################################################################################################################################
+ *
+ * Can you talk more about ::? What does it really mean? What is a good example of when we could use it for this game?
+ *
+ * What was the proper method of extending classes from one another? Is there an issue with my implementation?
+ *
+ * At the top of every class do you put every property you will use, or only those that will be returned by a function? Also, is it okay
+ * to assign those properties a value at the top of the class?
+ *
+ * Is it bad to have a method depend on another method running beforehand? What is the best way to account for this? Ex: $dealer->determineWinners()
+ * needs to run before $dealer->showWinners()
+ *
+ * I did not run an exception throw/catch because the structure of the Deck constructor builds a deck based on the allowedSuits variable. Is this
+ * okay? Is it bad to do this because information should be as deconstructed as possible, which would mean it should be made explicitly clear what
+ * information is going into building the deck outside of the class itself?
+ */
