777bb219755f3ec29c195b2e28de7483088d5338
Jared Selcoe <jared.orion.selcoe@gmail.com>
2015-10-13 19:52:34 -0500

    More practice using the terminal to add commits, and updated file 04-singleton

--- notes/day09/04-singleton.php
+++ notes/day09/04-singleton.php
@@ -1,7 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jaredselcoe
- * Date: 10/13/15
- * Time: 7:35 PM
- */
+
+class Database {
+    /**
+     * Instance of itself
+     * @var Database
+     */
+    private static $instance;
+
+    // Make the constructor so nobody can say $db = new Database(...);
+    private function __construct($host, $username, $password) {
+        // Connect to mysql
+        echo 'Connecting to mysql...<br />';
+        echo 'Host: ' . $host . ' User:' . $username . ' Pass:' . $password;
+        echo '<hr />';
+    }
+
+    public static function getInstance($host, $username, $password) {
+
+        // If the $instance variable is not set then create an instance of the database in the $instance variable
+        if(!isset(self::$instance)) {
+
+            // Identical to saying self::$instance = new Database($host, $username, $password)
+            self::$instance = new self($host, $username, $password);
+        }
+
+        return self::$instance;
+    }
+}
+
+
+/*
+ * THIS IS WHAT WE WOULD DO IF WE WERE NOT USING PRIVATE/STATIC (SINGLETON) DESIGN
+
+// Here is the facebook page
+$db = new Database('localhost', 'root', 'root');
+
+// A few thousand lines later
+$db = new Database('localhost', 'root', 'root');
+
+// In some other class
+$db = new Database('localhost', 'root', 'root');
+
+// ALL OF THIS CREATES MULTIPLE CONNECTIONS UNNECESSARILY
+
+*/
+
+
+// This will only create one instance of the connection despite being written multiple times
+$db = Database::getInstance('localhost', 'root', 'root');
+$db = Database::getInstance('localhost', 'root', 'root');
+$db = Database::getInstance('localhost', 'root', 'root');
